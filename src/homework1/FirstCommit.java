f44c2e479d8b5f8c8068959943a3f937943b713d
Ethan <ethan@MacBook-Pro-2.local>
2021-09-12 19:05:09 -0700

    Added javadoc to both the FirstCommit.java and SecondCommit.java files.

--- src/homework1/FirstCommit.java
+++ src/homework1/FirstCommit.java
@@ -1,14 +1,26 @@
 package homework1;
 
+/**
+ * A class for creating an instance variable and initializing in a constructor.
+ * @author ethan
+ *
+ */
 public class FirstCommit 
 {
 	private String name; 
 	
+	/**
+	 * Constructs a FirstCommit object with a name parameter
+	 * @param name the name of the person
+	 */
 	public FirstCommit(String name)
 	{
 		this.name = name;
 	}
 	
+	/**
+	 * To give a string representation of the FirstCommit object created
+	 */
 	@Override 
 	public String toString()
 	{
--- src/homework1/SecondCommit.java
+++ src/homework1/SecondCommit.java
@@ -1,13 +1,28 @@
 package homework1;
 
+/**
+ * A class for making a main method that will create an object 
+ * from the FirstCommit class and print the value of instance variable 
+ * of the object.
+ * @author ethan
+ *
+ */
 public class SecondCommit extends FirstCommit
 {
 
+	/**
+	 * Constructor that accesses the name variable of the FirstCommit parent class.
+	 * @param name name of the person
+	 */
 	public SecondCommit(String name)
 	{
 		super(name);
 	}
 	
+	/**
+	 * Creates an object of the FirstCommit class and prints its name.
+	 * @param args
+	 */
 	public static void main(String[] args)
 	{
 		FirstCommit object = new FirstCommit("Ethan");

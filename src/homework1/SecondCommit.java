2bbfed5467d2513882e3469f0a610b91fd2f4f57
Ethan <ethan@MacBook-Pro-2.local>
2021-09-12 18:54:28 -0700

    Created another Java class that has a public static main method, creates an object of the first class, and prints the value of the instance variable of the object.

---
+++ src/homework1/SecondCommit.java
@@ -0,0 +1,17 @@
+package homework1;
+
+public class SecondCommit extends FirstCommit
+{
+
+	public SecondCommit(String name)
+	{
+		super(name);
+	}
+	
+	public static void main(String[] args)
+	{
+		FirstCommit object = new FirstCommit("Ethan");
+		System.out.println(object);
+	}
+	
+}
